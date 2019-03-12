06c322f79d66c0d6a35778c4a7c31fd8130d55f8
sirjmkitavi <kitavimjoseph@gmail.com>
2019-03-12 15:58:57 +0300

    add MapView

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,11 +6,17 @@ import {
   StatusBar,
   Image,
   ScrollView,
+  Picker,
+  TouchableOpacity,
 } from 'react-native'
 import firebase from 'react-native-firebase'
+import MapView, { Marker } from 'react-native-maps'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import FAB from 'react-native-fab'
 
 import { policeLogo } from '../../assets/images'
 import Incident from './Incident'
+import { mapStyle } from '../../constants/mapStyles'
 
 class Home extends Component {
   static navigationOptions = ({ navigation }) => ({
@@ -31,6 +37,8 @@ class Home extends Component {
     super(props)
     this.state = {
       incidents: [],
+      mapView: false,
+      incidentType: '',
     }
   }
 
@@ -56,6 +64,23 @@ class Home extends Component {
     this.props.navigation.navigate('IncidentDetails', { incident })
   }
 
+  renderMarkers = () => {
+    let incidents = this.state.incidents
+    if(this.state.incidentType === '') {
+      return this.state.incidents.map(item =>
+        <Marker
+          coordinate={item.location.coords}
+        />
+        )
+    }
+
+    return incidents.filter(incident => incident.incidentType === this.state.incidentType)
+      .map(item =>
+          <Marker
+            coordinate={item.location.coords}
+          />
+        )
+  }
 
   render() {
     return (
@@ -64,10 +89,67 @@ class Home extends Component {
           backgroundColor='#000440'
           barStyle='light-content'
         />
-        
-        <ScrollView contentContainerStyle={{ margin: 10 }}>
-          {this.state.incidents.map((item) => <Incident incident={item} onPress={this.onIncidentPress} />)}
-        </ScrollView>
+
+        {!this.state.mapView ? (
+          <ScrollView contentContainerStyle={{ margin: 10 }}>
+            {this.state.incidents.map((item) => <Incident incident={item} onPress={this.onIncidentPress} />)}
+          </ScrollView>
+        ) : (
+          <MapView
+            style={{ ...StyleSheet.absoluteFillObject }}
+            customMapStyle={mapStyle}
+            showBackground={false}
+            listenKeyboard
+            showsMyLocationButton
+            showsUserLocation
+            initialRegion={{
+              latitude: 0.204084,
+              longitude: 37.809983,
+              latitudeDelta: 6,
+              longitudeDelta: 7,
+            }}
+          >
+            {this.renderMarkers()}
+          </MapView>
+        )}
+
+        <FAB
+          buttonColor='#000440'
+          iconTextColor="#FFFFFF"
+          onClickAction={() => this.setState(prevState => ({ mapView: !prevState.mapView }))}
+          visible={true}
+          iconTextComponent={
+          this.state.mapView ?
+            <MaterialCommunityIcons name='library-books' size={40} color='black' /> :
+            <MaterialCommunityIcons name='map-search' size={35} color='black' />
+          }
+        />
+
+        {this.state.mapView &&
+          <Picker
+            mode='dropdown'
+            selectedValue={this.state.incidentType}
+            style={{
+              position: 'relative',
+              elevation: 3,
+              marginRight: -240,
+              marginBottom: -420,
+              height: 40,
+              width: 150,
+              backgroundColor: 'rgba(255,255,255, 0.9)',
+            }}
+            onValueChange={(itemValue) => this.setState({ incidentType: itemValue })}
+          >
+            <Picker.Item label='Filter Incident Type...' value='' />
+            <Picker.Item label='Accident' value='Accident' />
+            <Picker.Item label='Speeding Violation' value='Speeding Violation' />
+            <Picker.Item label='DUI' value='DUI' />
+            <Picker.Item label='Safety Regulations' value='Safety Regulations' />
+            <Picker.Item label='PSV Offences' value='PSV Offences' />
+            <Picker.Item label='Documentation' value='Documentation' />
+            <Picker.Item label='Other' value='Other' />
+          </Picker>
+        }
       </View>
     )
   }
